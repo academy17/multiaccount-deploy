340700732e23ae2a4685e999d158da50d6a1abd2
academy17 <112261995+academy17@users.noreply.github.com>
2024-04-11 22:58:59 +0100

    multiaccountdeploy

---
+++ scripts/deployMultiAccount.ts
@@ -0,0 +1,57 @@
+import { ethers, run, upgrades } from "hardhat"
+import { Addresses, loadAddresses, saveAddresses } from "../utils/file"
+
+async function main() {
+	const [deployer] = await ethers.getSigners()
+
+	console.log("Deploying contracts with the account:", deployer.address)
+	let deployedAddresses: Addresses = loadAddresses()
+	console.log("Deployed Addresses:", deployedAddresses);
+
+	const SymmioPartyA = await ethers.getContractFactory("SymmioPartyA")
+
+	// Deploy SymmioPartyB as upgradeable
+	const Factory = await ethers.getContractFactory("MultiAccount")
+	console.log("Factory Deployed. ");
+
+	const admin = process.env.ADMIN_PUBLIC_KEY
+	const contract = await upgrades.deployProxy(Factory, [
+		admin, deployedAddresses.symmioAddress,
+		SymmioPartyA.bytecode,
+	], { 
+		initializer: "initialize",
+		timeout: 600000, // Example: extend timeout to 10 minutes
+		pollingInterval: 4000 // Example: check every 4 seconds
+	  })
+	await contract.deployed()
+	console.log("Contract Deployed. ");
+
+
+	const addresses = {
+		proxy: contract.address,
+		admin: await upgrades.erc1967.getAdminAddress(contract.address),
+		implementation: await upgrades.erc1967.getImplementationAddress(
+		  contract.address,
+		),
+	}
+	console.log(addresses)
+
+	deployedAddresses.multiAccountAddress = contract.address
+	saveAddresses(deployedAddresses)
+
+	try {
+		console.log("Verifying contract...")
+		await new Promise((r) => setTimeout(r, 15000))
+		await run("verify:verify", { address: addresses.implementation })
+		console.log("Contract verified!")
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+	  console.error(error)
+	  process.exit(1)
+  })
